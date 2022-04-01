10793218f42181cd4e2d94b368fff94e61a6089f
Dale LaPlante <bmbmjmdm@gmail.com>
2022-04-01 15:08:07 +0200

    redo icons

--- FE/hear_you_out/app/BottomButtons.tsx
+++ FE/hear_you_out/app/BottomButtons.tsx
@@ -15,6 +15,10 @@ import Check from './Check.png'
 import TutorialElement from './TutorialElement';
 import { SizeContext } from './helpers'
 import { getBBLargeSize, getBBSmallSize, resizeBBLarge, resizeBBSmall } from './helpers'
+import Garbage from './garbage.png'
+import ThumbsDown from './thumbsDown.png'
+import ThumbsUp from './thumbsUp.png'
+import RightArrow from './rightArrow.png'
 
 type BottomButtonsProps = {
   theme: "answer" | "question",
@@ -50,24 +54,24 @@ const Button = ({name, theme, onPress, isInTutorial, currentTutorialElement, onT
   const xCalloutDistance = theme === 'question' ? -300 : -300
 
   if (name === "check") {
-    const bgColor = theme == "question" ? "#FF8AB2" : "#ABFFB8"
-    const pressedColor = theme == "question" ? "#C16A89" : "#598560"
+    const bgColor = theme === "question" ? "#FF8AB2" : "#ABFFB8"
+    const pressedColor = theme === "question" ? "#C16A89" : "#598560"
     radius = getBBLargeSize(screenSize)
     underlay = pressedColor
     style = [styles.bigCircle, resizeBBLarge(screenSize), {backgroundColor: pressed ? underlay : bgColor}, pressed ? styles.depth : {}]
-    image = Check
-    imageSize = (11/20) * radius
+    image = theme === "question" ? RightArrow : ThumbsUp
+    imageSize = theme === "question" ? (14/20) * radius : (13/20) * radius
     calloutText = theme === 'question' ? "When you're ready to submit you answer, press this!" : "When you're ready to proceed to the next answer, you can use one of these bottom buttons or swipe. Use this button or swipe right if you agree with the answer"
     calloutDistance = theme === 'question' ? -300 : -400
   }
   else if (name === "x") {
-    const bgColor = theme == "question" ? "#FFC5C3" : "#ABFFB8"
-    const pressedColor = theme == "question" ? "#C69A99" : "#598560"
+    const bgColor = theme === "question" ? "#FFC5C3" : "#ABFFB8"
+    const pressedColor = theme === "question" ? "#C69A99" : "#598560"
     radius = getBBLargeSize(screenSize)
     underlay = pressedColor
     style = [styles.bigCircle, resizeBBLarge(screenSize), {backgroundColor: pressed ? underlay : bgColor}, pressed ? styles.depth : {}]
-    image = X
-    imageSize = 0.5 * radius
+    image = theme === "question" ? Garbage : ThumbsDown
+    imageSize = theme === "question" ? (12/20) * radius : (13/20) * radius
     calloutText = theme === 'question' ? "If you don't like what you've recorded, press this to delete your recording and start over" : "Use this button or swipe left if you disagree with the answer"
     calloutDistance = xCalloutDistance
   }
@@ -76,7 +80,7 @@ const Button = ({name, theme, onPress, isInTutorial, currentTutorialElement, onT
     underlay = '#918a64'
     style = [styles.littleCircle, resizeBBSmall(screenSize), pressed ? {backgroundColor: underlay} : {}, pressed ? styles.depthSmall : {}]
     image = theme == "question" ? Speaker : Skip;
-    imageSize = (2/3) * radius
+    imageSize =  theme === "question" ? (2/3) * radius : (3/4) * radius 
     calloutText = theme === 'question' ? "To hear back what you've already recorded, press here" : "Use this button if you aren't sure whether you agree or not"
     calloutDistance = xCalloutDistance + (getBBLargeSize(screenSize) - getBBSmallSize(screenSize)) / 2
   }

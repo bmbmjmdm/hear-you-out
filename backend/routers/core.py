350d60d2be8652d6aa927f6e0bc0619f8d433754
teleoflexuous <teleoflexuous@proton.me>
2023-12-26 09:36:31 +0100

    default behaviour of /answers/ set to single of_the_day

--- backend/routers/core.py
+++ backend/routers/core.py
@@ -66,6 +66,49 @@ async def get_question_of_the_day(
     return question
 
 
+@router.get("/answers", response_model=List[schemas.AnswerExternalModel])
+async def get_answers(
+    user: Annotated[models.User, Depends(authentication.get_current_active_user)],
+    db: AsyncSession = Depends(get_db),
+    seen_answers_ids: List[uuid.UUID] = Query(None),
+    limit: int = Query(1),
+    ids: List[uuid.UUID] = Query(None),
+    questions_ids: List[uuid.UUID] = Query(None),
+):
+    answers_CRUD = Answer.CRUDAnswer(db, models.Answer)
+    question_CRUD = CRUDObject(db, models.Question)
+    kwargs = {}
+
+    if seen_answers_ids is not None:
+        limit += len(seen_answers_ids)
+
+    if questions_ids is None:
+        question = await question_CRUD.get(query_dict={"of_the_day": True})
+        if question is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="No questions of the day found",
+            )
+        kwargs["question_id"] = question.id
+    else:
+        kwargs["question_id"] = questions_ids
+    if ids is not None:
+        kwargs["id"] = ids
+    answers = await answers_CRUD.get_multi(as_pydantic=True, limit=limit, **kwargs)
+
+    if seen_answers_ids is not None:
+        answers = [
+            answer
+            for answer in answers
+            if answer["id"] not in [str(id) for id in seen_answers_ids]
+        ]
+    
+    for answer in answers:
+        await answers_CRUD.view(answer, user, as_pydantic=True)
+    check_list_length(answers)
+    return answers
+
+
 @router.post("/answer", response_model=schemas.AnswerExternalModel)
 async def submit_answer(
     user: Annotated[models.User, Depends(authentication.get_current_active_user)],
@@ -76,6 +119,18 @@ async def submit_answer(
     # CRUD create: return answer, audio_data
     answer = await answers_CRUD.create(answer, as_pydantic=True)
     return answer
+
+
+@router.get("/answers/views", response_model=List[schemas.AnswerExternalViewsModel])
+async def get_answers_views(
+    user: Annotated[models.User, Depends(authentication.get_current_active_user)],
+    db: AsyncSession = Depends(get_db),
+    ids: List[uuid.UUID] = Query(None),
+):
+    answers_CRUD = Answer.CRUDAnswer(db, models.Answer)
+    answers = await answers_CRUD.get_views_multi(id=ids, as_pydantic=True)
+    check_list_length(answers)
+    return answers
 
 
 @router.post("/answer/view", response_model=schemas.AnswerExternalModel)
@@ -96,36 +151,6 @@ async def submit_answer_view(
     answer = await answers_CRUD.view(answer, user, as_pydantic=True)
     print(f"answer: {answer}")
     return answer
-
-
-@router.get("/answers", response_model=List[schemas.AnswerExternalModel])
-async def get_answers(
-    user: Annotated[models.User, Depends(authentication.get_current_active_user)],
-    db: AsyncSession = Depends(get_db),
-    ids: List[uuid.UUID] = Query(None),
-    questions_ids: List[uuid.UUID] = Query(None),
-):
-    answers_CRUD = Answer.CRUDAnswer(db, models.Answer)
-    kwargs = {}
-    if questions_ids is not None:
-        kwargs["question_id"] = questions_ids
-    if ids is not None:
-        kwargs["id"] = ids
-    answers = await answers_CRUD.get_multi(as_pydantic=True, **kwargs)
-    check_list_length(answers)
-    return answers
-
-
-@router.get("/answers/views", response_model=List[schemas.AnswerExternalViewsModel])
-async def get_answers_views(
-    user: Annotated[models.User, Depends(authentication.get_current_active_user)],
-    db: AsyncSession = Depends(get_db),
-    ids: List[uuid.UUID] = Query(None),
-):
-    answers_CRUD = Answer.CRUDAnswer(db, models.Answer)
-    answers = await answers_CRUD.get_views_multi(id=ids, as_pydantic=True)
-    check_list_length(answers)
-    return answers
 
 
 # Flag answer

d64bfab4621a379203067a0688cf35464ee912c3
teleoflexuous <teleoflexuous@proton.me>
2024-01-29 06:37:39 +0100

    bunch of stuff

--- backend/authentication.py
+++ backend/authentication.py
@@ -17,7 +17,7 @@ import schemas
 from database import get_db
 from config import config
 
-from CRUD import TestGroup, Test
+from CRUD import TestGroup, Test, User
 
 #
 ACCESS_TOKEN_EXPIRE_MINUTES = int(config.ACCESS_TOKEN_EXPIRE_MINUTES)
@@ -38,26 +38,21 @@ async def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
-async def get_user(
+async def get_user_by_username(
     db: AsyncSession, username: str
 ) -> models.User:  # This could be in CRUD
-    stmt = (
-        select(models.User)
-        .where(models.User.username == username)
-        .options(
-            selectinload(models.User.answers_viewed),
-            selectinload(models.User.answers_authored),
-        )
+    userCRUD = User.CRUDUser(db)
+    user = await userCRUD._retrieve(
+        query_dict={"username": username},
+        related=["answers_authored", "answers_viewed", "topic_subscriptions"],
     )
-    result = await db.execute(stmt)
-    user = result.scalars().first()
-    return user
+    return user[0] if len(user) > 0 else None
 
 
 async def authenticate_user(
     db: AsyncSession, username: str, password: str
 ) -> models.User:
-    user = await get_user(db, username)
+    user = await get_user_by_username(db, username)
     if not user:
         return False
     if not await verify_password(password, user.password):
@@ -99,7 +94,7 @@ async def get_current_user(
         token_data = schemas.TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = await get_user(db, username=token_data.username)
+    user = await get_user_by_username(db, username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
@@ -127,7 +122,6 @@ async def register_user(
     user: schemas.UserCreateModel,
     db: AsyncSession = Depends(get_db),
 ) -> models.User:
-    
     # Check if groups are valid
     test_CRUD = Test.CRUDTest(db, models.Test)
     for test_group in user.test_groups:
@@ -195,7 +189,7 @@ async def register_user(
             status_code=400,
             detail=f"Error registering user",
         )
-    created_user = await get_user(db, username=user.username)
+    created_user = await get_user_by_username(db, username=user.username)
 
     test_CRUD = Test.CRUDTest(db, models.Test)
     test_group_CRUD = TestGroup.CRUDTestGroup(db, models.TestGroup)
--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -23,7 +23,9 @@ from datetime import timedelta
 
 import models, schemas
 from database import get_db
-import authentication 
+import authentication
+from CRUD.User import CRUDUser
+
 
 class Message(BaseModel):
     message: str
@@ -34,6 +36,7 @@ router = APIRouter(
     tags=["auth"],
     responses={404: {"description": "Not found"}},
 )
+
 
 @router.post("/register", response_model=schemas.UserExternalModel)
 async def register_user(
@@ -47,47 +50,57 @@ async def register_user(
     user = schemas.UserExternalModel.model_validate(user)
     return user
 
+
 @router.get("/users/me", response_model=schemas.UserExternalModel)
 async def get_user(
-    current_user: Annotated[models.User, Depends(authentication.get_current_active_user)],
+    current_user: Annotated[
+        models.User, Depends(authentication.get_current_active_user)
+    ],
 ):
     return schemas.UserExternalModel.model_validate(current_user)
+
 
 # 2 Login modes:
 # 1) Login with device ID. Default, less secure
 # 2) Login with username and password. Required for admin
 
+
 @router.post("/login", response_model=schemas.Token)
 async def login_device(
     device_id: str,
+    firebase_token: str,
     db: AsyncSession = Depends(get_db),
 ):
     # Get user from database
-    user = await db.execute(
-        select(models.User).where(models.User.device_id == device_id).options(
-            selectinload(models.User.answers_authored),
-            selectinload(models.User.answers_viewed),
-        )
+    userCRUD = CRUDUser(db)
+    user = await userCRUD.get_one(
+        query_dict={"device_id": device_id}, related=["topic_subscriptions"]
     )
-    user = user.unique().scalars().first()
-
     if user is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="No user found",
         )
-    
-    if user.is_admin:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Admins must login with username and password",
-        )
+
+    # if user.is_admin:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_401_UNAUTHORIZED,
+    #         detail="Admins must login with username and password",
+    #     )
 
     access_token_expires = timedelta(minutes=authentication.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = await authentication.create_access_token(
         data={"sub": user.device_id}, expires_delta=access_token_expires
     )
+    if user.firebase_token != firebase_token:
+        user = await userCRUD.update(
+            user_id=user.id,
+            user=schemas.UserUpdateModel(
+                id=user.id, device_id=user.device_id, firebase_token=firebase_token
+            ),
+        )
     return {"access_token": access_token, "token_type": "bearer", "user_id": user.id}
+
 
 @router.post("/login/username", response_model=schemas.Token)
 async def login_username(
